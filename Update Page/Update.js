bd9abbf0b82de9d766468fc2d2abfdc6531bd2e2
ayaahhehab <aya.ehab2001@gmail.com>
2022-04-30 22:57:21 +0200

    final

--- Update Page/Update.js
+++ Update Page/Update.js
@@ -97,4 +97,8 @@ function getConfirmation() {
         return false;
     }
 }
+window.addEventListener("scroll",function(){
+    var header = document.querySelector("header");
+    header.classList.toggle("sticky",window.scrollY > 0)
+})
  
