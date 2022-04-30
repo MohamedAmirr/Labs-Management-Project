1dfe13099ea85dcd159a4ee3d97d996b3c9ad419
MohamedAmirr <m7mdd.amirr@gmail.com>
2022-04-30 03:12:47 +0200

    hoba

--- cover/index-java.js
+++ cover/index-java.js
@@ -1,14 +1,14 @@
 function reveal(){
-    var reveals=document.querySelectorAll(".transition");
-    for (var i=0;i<reveals.length;i++){
+    var transitions=document.querySelectorAll(".transition");
+    for (var i=0;i<transitions.length;i++){
         var windowHeight=window.innerHeight;
-        var elementTop=reveals[i].getBoundingClientRect().top;
+        var elementTop=transitions[i].getBoundingClientRect().top;
         var elementVisible=300;
         if(elementTop<windowHeight-elementVisible){
-            reveals[i].classList.add("active");
+            transitions[i].classList.add("active");
         }
         else {
-            reveals[i].classList.remove("active");
+            transitions[i].classList.remove("active");
         }
     }
 }
